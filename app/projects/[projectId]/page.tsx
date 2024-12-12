daca2ceb38ebd5f7c18e1ff671296c6fd8a862c2
nhinhampro <109505529+nhinhampro@users.noreply.github.com>
2024-12-12 23:39:54 +0700

    Update page.tsx

--- app/projects/[projectId]/page.tsx
+++ app/projects/[projectId]/page.tsx
@@ -31,9 +31,14 @@ export default async function ProjectPage({ params }: any) {
         <div className={'flex flex-row items-center justify-between'}>
           <div className={'flex flex-row items-center'}>
            <img
-                  className={'h-12 w-12 rounded-full'}
+                           className={'mr-4 h-12 w-12 rounded-full'}
+              src={
+                'https://innostudio.de/fileuploader/images/default-avatar.png'
+              }
+              alt={'user'}
+{/*                   className={'h-12 w-12 rounded-full'}
                   src={gravatar.url(project.author.email)}
-                  alt={project.author.fullName}
+                  alt={project.author.fullName} */}
             />
             <div className={'my-4'}>
               <div>{project.author.fullName}</div>
